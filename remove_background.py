5b47d1b444c12f6e33d3a8e758c6b961e736e184
Yejoon Lee <beeriver1@gmail.com>
2022-11-08 23:18:16 +0900

    [FEAT] background fill with interpolation, using masks from rembg
    also, new background image

--- remove_background.py
+++ remove_background.py
@@ -3,16 +3,15 @@ import cv2
 import numpy as np
 from rembg import remove
 
-
-def fill_background(image : np.ndarray, background: np.ndarray) -> np.ndarray:
+def fill_background(image : np.ndarray, background: np.ndarray, mask: np.ndarray, mask_threshold : int) -> np.ndarray:
     background_resized = cv2.resize(background, (image.shape[1], image.shape[0]))
 
     result = image.copy()
 
-    # find where all 3 channels of image are 0
-    # then fill with background_resized with the same coordinates
-    for x, y in np.argwhere(np.all(image == 0, axis=-1)):
-        result[x, y, :] = background_resized[x, y, :]
+    # interpolate between image and background; weight is given from mask
+    for x, y in np.argwhere(mask <= mask_threshold):
+        image_weight = mask[x, y] / 255 # value between 0 and 1
+        result[x, y, :] = (1-image_weight) * background_resized[x, y, :] + image_weight * image[x, y, :]
     
     return result
 
@@ -20,18 +19,22 @@ def fill_background(image : np.ndarray, background: np.ndarray) -> np.ndarray:
 def main():
     image_path = sys.argv[1]
     background_path = sys.argv[2] if len(sys.argv) > 2 else None
+    mask_threshold = int(sys.argv[3]) if len(sys.argv) > 3 else 255 # mask.max() = 255
     image = cv2.imread(image_path)
 
+    # remove background
     result = remove(image)   
     cv2.imwrite("result/background_removed.png", result)
 
+    # fill background
     if background_path is not None:
         image = cv2.imread("result/background_removed.png")
         background = cv2.imread(background_path)
-        result = fill_background(image, background)
+        mask = remove(image, only_mask=True)
 
-        # write array to image
-        cv2.imwrite("result/background_filled.png", result)
+        result = fill_background(image, background, mask, mask_threshold)
+
+        cv2.imwrite(f"result/background_filled_mask_{mask_threshold}.png", result)
 
 if __name__ == '__main__':
     main()
